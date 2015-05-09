5b831e63cfbf61c017ff4d65e451180a83623cf6
robert lee <100LL@users.noreply.github.com>
2015-05-09 09:29:18 -0700

    clean up the formatting

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,6 @@
 ## repeated calculation of the inverse can be avoided
 
 ## special "matrix" object that stores a matrix and its inverse
-
 makeCacheMatrix <- function(x = matrix()) {
 	inv <- NULL
 	set <- function(y) {
@@ -13,15 +12,15 @@ makeCacheMatrix <- function(x = matrix()) {
 	setinverse <- function(solve) inv <<- solve
 	getinverse <- function() inv
 	list(set = set,
-		get = get,
-		setinverse = setinverse,
-		getinverse = getinverse) 
+	     get = get,
+	     setinverse = setinverse,
+	     getinverse = getinverse
+	) 
 }
 
 
-## return the inverse of the matrix object.
+## return the inverse of the matrix object,
 ## fetched from cache or calculated if not cached.
-
 cacheSolve <- function(x, ...) {
 	## Return a matrix that is the inverse of 'x'
 	inv <- x$getinverse()
@@ -30,7 +29,7 @@ cacheSolve <- function(x, ...) {
 		return(inv)
 	}
 	data <- x$get()
-	inv <- solve(data,...)
+	inv <- solve(data, ...)
 	x$setinverse(inv)
 	inv
 }
